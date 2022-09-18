193e70fe42556ba5ef0907aea396f226cc894bef
Nehorai Hadad <Nehorai.hadad@gmail.com>
2022-09-18 16:23:25 +0300

    bug fix (login renders rYour branch is up tossate with 'origin/management'.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const isAuth = require('./authMiddleware').isAuth;
 
-// !! uses in tests only. usually rendering => /users/login => /index !!
 /* GET home page. */
 router.get('/', isAuth, function(req, res, next) {
     res.render('index', {name: ''});  //uses EJS to show name user
